e3d6b7f3c8453108c0746d97124ddb844a3bb365
johang2012 <johan.gudmundsson2012@gmail.com>
2017-10-19 20:48:39 +0200

    Add server

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const express = require('express');
+const request = require('request');
+const { promisify } = require('util');
+const requestAsync = promisify(request);
+
+const app = express();
+const port = 8080;
+const services = [];
+
+process.argv.forEach((service, i) => {
+  if (i > 1) {
+    services.push(service);
+  }
+});
+
+services.map(service => requestAsync(service));
+
+app.get('/', (req, res) => {
+  Promise.all(services)
+    .then(response => {
+      res.json({ a: response[0], b: response[1]})
+    })
+});
+
+app.listen(port);
